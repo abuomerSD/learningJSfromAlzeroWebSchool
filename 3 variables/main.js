764dfbd58e6ba09a5a3cd3b4ceb2c8b84c6371b0
Eltayeb <tayeb1293@yahoo.com>
2022-07-18 06:09:53 +0200

    variables lesson

---
+++ 3 variables/main.js
@@ -0,0 +1,20 @@
+// JavaScript is a lossley type language
+// JavaScript is case senstive
+// JavaScript uses camelCase do identefy the variables
+// every var variable will be in the window object
+
+var username = "Eltayeb";
+
+// console.log(username);
+// console.log(username);
+// console.log(username);
+// console.log(hello);
+
+hello.innerHTML = username ;
+
+
+let password = "113123ddsd";
+
+const email =  "saseew@yahoo.com";
+
+
