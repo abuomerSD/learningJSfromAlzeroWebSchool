ca58e7829b79c55e5bc6a9c6bb6eae89646b59cf
Eltayeb <tayeb1293@yahoo.com>
2022-07-19 06:28:37 +0200

    Arthmetic Operators

---
+++ 5 Arthmetic Operators/main.js
@@ -0,0 +1,17 @@
+console.log(10 + 20);
+
+console.log(10 + "osama");
+
+console.log(10 - 5);
+
+console.log(2 ** 4);
+
+console.log(10 - "saew");
+
+console.log(typeof NaN);
+
+// increment
+
+// pre
+
+// post
