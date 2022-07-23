7c13a82db2212272001e2087e958cd4e5ee7d1e0
Eltayeb <tayeb1293@yahoo.com>
2022-07-23 06:46:18 +0200

    Numbers

--- 6 unary plus and negation operators/main.js
+++ 6 unary plus and negation operators/main.js
@@ -30,3 +30,17 @@ x -= 20;
 x /= 10;
 
 console.log(x);
+
+// Numbers
+
+console.log("******************");
+
+console.log(1000000);
+console.log(1_000_000);
+console.log(1e6);
+console.log(10 ** 6);
+
+console.log(1000.0);
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
