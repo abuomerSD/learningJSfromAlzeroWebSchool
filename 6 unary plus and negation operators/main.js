55dc063da31727e3ec005de292a74ab4d22d1020
Eltayeb <tayeb1293@yahoo.com>
2022-07-23 06:31:35 +0200

    unary operators

---
+++ 6 unary plus and negation operators/main.js
@@ -0,0 +1,19 @@
+console.log(+100);
+console.log(+"-100");
+console.log(-"osama");
+
+console.log(-"-100");
+
+console.log(Number("100"));
+
+// type coercion
+
+let a = "10";
+let b = 20;
+let c = true;
+
+console.log("========================");
+
+console.log(+a + b);
+
+console.log(false - true);
