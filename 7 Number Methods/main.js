ce7acd00d175121eaadff1943f2938e55fd81dac
Eltayeb <tayeb1293@yahoo.com>
2022-07-24 05:14:16 +0200

    Number Methods

---
+++ 7 Number Methods/main.js
@@ -0,0 +1,23 @@
+// Number Methods
+
+console.log((100).toString());
+
+//console.log(100..toString);
+
+console.log((100.5444).toFixed(2));
+
+console.log(parseInt("100 osama"));
+
+console.log(parseFloat("100.500 osama"));
+
+console.log(Number.isInteger("100"));
+
+console.log(Number.isInteger(100.545));
+
+console.log(Number.isInteger(100));
+
+console.log(Number.isNaN("osama"));
+
+/**
+ * Math Object
+ */
